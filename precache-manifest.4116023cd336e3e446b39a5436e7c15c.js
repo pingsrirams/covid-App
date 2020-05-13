40793cfbb32a856ff49f18f0e255bdc17fe6a771
Sriram <pingsrirams@gmail.com>
2020-05-12 23:23:36 -0400

    Updates

---
+++ precache-manifest.4116023cd336e3e446b39a5436e7c15c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fad9518e510cbc13df01e3310589f68",
+    "url": "/covid-App/index.html"
+  },
+  {
+    "revision": "dabcca2c91bd5004a4cb",
+    "url": "/covid-App/static/css/main.3a569c8b.chunk.css"
+  },
+  {
+    "revision": "ebf38f90842619367a12",
+    "url": "/covid-App/static/js/2.21a2d433.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid-App/static/js/2.21a2d433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dabcca2c91bd5004a4cb",
+    "url": "/covid-App/static/js/main.22df8c12.chunk.js"
+  },
+  {
+    "revision": "d01edb093c99e942694c",
+    "url": "/covid-App/static/js/runtime-main.330f1e11.js"
+  },
+  {
+    "revision": "ecb5516780c164d63361c9d3a141a602",
+    "url": "/covid-App/static/media/covid19-white-logo.ecb55167.png"
+  }
+]);
